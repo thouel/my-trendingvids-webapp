3075ae762be6c0a1df945394769c93a422efd002
thouel <t.houel@gmail.com>
2023-10-19 09:50:49 +0200

    ui: on mobile device, close the main menu when url changes

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -17,6 +17,10 @@ export default function Navbar() {
   const [queryString, setQueryString] = useState(searchParams.get('q') ?? '');
   const [menuIsOpen, setMenuIsOpen] = useState(false);
 
+  useEffect(() => {
+    setMenuIsOpen(false);
+  }, [pathname, searchParams]);
+
   const createQueryString = () => {
     const params = new URLSearchParams(searchParams);
     params.set('q', queryString);
@@ -66,34 +70,7 @@ export default function Navbar() {
           </span>
         </Link>
         <div className='flex md:order-2'>
-          {/* <button
-            type='button'
-            data-collapse-toggle='navbar-search'
-            aria-controls='navbar-search'
-            aria-expanded='false'
-            className='md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1'
-            onClick={toggleMenu}
-          >
-            <svg
-              className='w-5 h-5'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 20 20'
-            >
-              <path
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z'
-              />
-            </svg>
-            <span className='sr-only'>Search</span>
-          </button> */}
-
-          {/* Dark/Light theme button */}
-          {/* <div className='mr-3 pt-1 pb-1 pl-2 pr-2 rounded-md text-gray-600 border-gray-200 bg-gray-100 hover:text-orange-600'> */}
+          {/* Theme button (Dark/Light) */}
           <div className='inline-flex items-center p-2 mr-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'>
             <ThemeButton />
           </div>
