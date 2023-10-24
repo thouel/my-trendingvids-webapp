829c7578248273c8f44851acf370924933580cf9
thouel <t.houel@gmail.com>
2023-10-24 10:10:19 +0200

    ui: Added a back button on signout page

--- app/components/SignOut.jsx
+++ app/components/SignOut.jsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export default function SignOut() {
+  const router = useRouter();
   const handleSubmit = (e) => {
     e.preventDefault();
     signOut({ callbackUrl: '/' });
@@ -16,13 +18,25 @@ export default function SignOut() {
         className='flex flex-col w-full max-w-sm p-2 text-center border rounded shadow-lg'
       >
         <p>Are you sure you want to sign out?</p>
-        <button
-          type='submit'
-          className='py-1 mt-3 font-semibold text-gray-100 bg-orange-600 rounded shadow-md hover:shadow-lg'
-          tabIndex={1}
-        >
-          Sign out
-        </button>
+        <div className='flex flex-row gap-2'>
+          <button
+            type='button'
+            className='w-full py-1 mt-3 font-semibold text-orange-600 bg-gray-100 rounded shadow-md hover:shadow-lg'
+            tabIndex={1}
+            onClick={() => {
+              router.back();
+            }}
+          >
+            Back
+          </button>
+          <button
+            type='submit'
+            className='w-full py-1 mt-3 font-semibold text-gray-100 bg-orange-600 rounded shadow-md hover:shadow-lg'
+            tabIndex={2}
+          >
+            Sign out
+          </button>
+        </div>
       </form>
     </div>
   );
