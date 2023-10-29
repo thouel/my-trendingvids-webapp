9ae454698b120591891d0149943ac619b5908209
thouel <t.houel@gmail.com>
2023-10-29 09:43:40 +0100

    ui: fixed modal loading not properly showing

--- app/@modal/(.)show/[showType]/[id]/loading.js
+++ app/@modal/(.)show/[showType]/[id]/loading.js
@@ -4,9 +4,19 @@ import 'react-loading-skeleton/dist/skeleton.css';
 export default function ShowLoadingPage() {
   return (
     <>
-      <div>
-        <Skeleton count={1} height={'100px'} inline={true} className='mb-1' />
-        <Skeleton count={3} />
+      <div className='fixed inset-0 z-20 bg-black/70' aria-hidden='true' />
+      <div className='fixed inset-0 z-30 w-screen overflow-y-auto'>
+        <div className='flex items-center justify-center min-h-full p-4 text-center sm:p-0'>
+          <div className='relative overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl dark:bg-gray-950 sm:my-8 sm:w-full sm:max-w-2xl'>
+            <Skeleton
+              count={1}
+              height={'300px'}
+              inline={true}
+              className='mb-1'
+            />
+            <Skeleton count={3} />
+          </div>
+        </div>
       </div>
     </>
   );
