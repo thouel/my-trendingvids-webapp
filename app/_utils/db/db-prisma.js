c8e30b7ad3a38d39bd5a699a43c09e285689363b
thouel <t.houel@gmail.com>
2023-10-29 09:44:25 +0100

    func: added prisma log

--- app/_utils/db/db-prisma.js
+++ app/_utils/db/db-prisma.js
@@ -3,8 +3,31 @@ import { PrismaClient } from '@prisma/client';
 let prisma;
 
 if (!global.prisma) {
-  global.prisma = new PrismaClient();
+  global.prisma = new PrismaClient({
+    log: [
+      {
+        emit: 'event',
+        level: 'query',
+      },
+      {
+        emit: 'stdout',
+        level: 'error',
+      },
+      {
+        emit: 'stdout',
+        level: 'info',
+      },
+      {
+        emit: 'stdout',
+        level: 'warn',
+      },
+    ],
+  });
 }
 prisma = global.prisma;
+
+prisma.$on('query', (e) => {
+  console.log('Query: ' + e.query);
+});
 
 export default prisma;
