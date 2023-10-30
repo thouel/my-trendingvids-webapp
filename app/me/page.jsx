69fbd8dad29e03b34a969a01b63343b2d6a5c53f
thouel <t.houel@gmail.com>
2023-10-30 17:02:45 +0100

    chores: lint

--- app/me/page.jsx
+++ app/me/page.jsx
@@ -1,5 +1,6 @@
 import { options } from 'app/api/auth/[...nextauth]/options';
 import { getServerSession } from 'next-auth';
+import Image from 'next/image';
 
 export default async function MePage() {
   const session = await getServerSession(options);
@@ -9,10 +10,12 @@ export default async function MePage() {
       <h1>Hello {session.user.name}</h1>
       <h2>{session.user.email}</h2>
       <h3>
-        <img
+        <Image
           src={session.user.image}
           alt={`profile pic not found: ${session.user.image}`}
           title='profile picture'
+          width={200}
+          height={200}
         />
       </h3>
     </>
