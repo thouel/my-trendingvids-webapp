2070fdb474609b3948f2b7bc3fe58f33381770b7
thouel <t.houel@gmail.com>
2023-11-02 20:28:54 +0100

    cicd: headless variable for playwright without xserver

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     /* headless if running in CI */
-    headless: process.env.CI ? false : true,
+    headless: process.env.CI ? true : false,
   },
 
   /* Configure projects for major browsers */
