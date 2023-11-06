26266cf3ef13739fa29e4221b74cd37c30851e11
thouel <t.houel@gmail.com>
2023-11-06 16:41:38 +0100

    cicd: updated dropDatabase script

--- dropDatabase.js
+++ dropDatabase.js
@@ -1,36 +1,43 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 require('dotenv').config({ path: '.env.local' });
 
-async function dropDatabase() {
-  console.log('Drop database --- START');
-  try {
-    const client = new MongoClient(process.env.MONGODB_URI, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
-    });
+const uri = process.env.MONGODB_URI;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
-    const res = client.db().dropDatabase();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!',
+    );
 
+    // Create the Index
+    const result = await client.db().dropDatabase();
+    console.log('Database dropped', { result });
+  } finally {
+    // Ensures that the client will close when you finish/error
     await client.close();
-
-    console.log('Database dropped:', { res });
-  } catch (e) {
-    console.log('Drop database ERROR', { e });
-    return 1;
   }
-  console.log('Drop database --- END');
   return 0;
 }
 
-var res = 1;
 (async () => {
-  res = await dropDatabase();
+  const res = await run().catch((e) => {
+    console.dir(e);
+    return 1;
+  });
+  console.log('res', { res });
+  process.exit(res);
 })();
-
-process.exit(res);
