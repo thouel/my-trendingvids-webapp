22462f8b5541371f00d844ce33c9ea051888187b
thouel <t.houel@gmail.com>
2023-11-11 08:54:11 +0100

    chores: remove unnecessary route

--- app/api/shows/me/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { getMyShows } from 'app/utils/db/shows';
-
-export async function POST(req) {
-  const body = await req.json();
-  const { userId } = body;
-  const res = await getMyShows(userId);
-  return NextResponse.json(res);
-}
